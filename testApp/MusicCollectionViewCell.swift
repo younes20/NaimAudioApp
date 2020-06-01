4219a063240037e082a2b7a18ab9063f8959b4d1
Younes Nouri Soltan <>
2020-06-01 01:58:38 +0100

    first commit

---
+++ testApp/MusicCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MusicCollectionViewCell.swift
+//  naimAudioApp
+//
+//  Created by Younes Nouri Soltan on 31/05/2020.
+//  Copyright © 2020 Younes Nouri Soltan. All rights reserved.
+//
+
+import UIKit
+
+class MusicCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ testApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by Younes Nouri Soltan on 29/05/2020.
+//  Copyright © 2020 Younes Nouri Soltan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
